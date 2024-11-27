c60b195d8e6a01cb5d93048e612b510a70d1e237
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-11-28 01:46:48 +0545

    initial commits

---
+++ 2023/day1/main.js
@@ -0,0 +1,10 @@
+// Advent of Code 2023 - Day 1 
+//
+// STEPS : 
+// 1. read the file and get the data
+// 2. store the data into an anrray
+// 3. for each element in the data, seperate the first and last number, and join them. so, ab1dc3dc will be 13
+// 4. Store the data int a new array
+// 5. Count the sum of the array
+// 6. return the sum
+
