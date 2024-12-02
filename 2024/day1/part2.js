bc0c584b2bccbd461b60ebd45d2c82c758cf0244
adarshkunwar <adarshkunwarofficial@gmail.com>
2024-12-03 00:03:38 +0545

    feat: day 1 2024

---
+++ 2024/day1/part2.js
@@ -0,0 +1,29 @@
+import fs from "fs";
+// reading input from file
+const input = fs.readFileSync("input.txt", "utf-8");
+
+// breaking the input into two seperate array
+const breakArrayBasedOnLines = input.trim().split("\n");
+const brokenSide1 = breakArrayBasedOnLines.map((el) => el.split(" ")[0]);
+const brokenSide2 = breakArrayBasedOnLines.map((el) => el.split(" ")[3]);
+
+const sortedSide1 = brokenSide1.sort((a, b) => a - b);
+const sortedSide2 = brokenSide2.sort((a, b) => a - b);
+
+// find the difference bwetween each element
+
+let diff = 0;
+let i = 0;
+// const max = 5;
+const max = sortedSide1.length;
+while (i < max) {
+  const similarityScore =
+    sortedSide1[i] * sortedSide2.filter((el) => el === sortedSide1[i]).length;
+  diff += similarityScore;
+  console.log(similarityScore, "1", sortedSide1[i], "2", sortedSide2[i]);
+  i++;
+}
+
+console.log(diff);
+
+console.log("----------");
